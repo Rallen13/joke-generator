237e425347fdfce4c4a60148b3a4786f48358666
Rachel Allen <rallenco13@gmail.com>
2022-07-17 13:58:07 -0600

    set up component architecture

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      jokes: [],
+      jokes: [
+        {
+          id: 1,
+          joke: "What do you get when you cross a snail with a porcupine?",
+          punchline: "A slowpoke!",
+        },
+        {
+          id: 2,
+          joke: "What kind of tree can fit in one hand?",
+          punchline: "A palm tree!",
+        },
+        {
+          id: 3,
+          joke: "How do they keep the basketball arena cool?",
+          punchline: "THey fill it with fans!",
+        },
+      ],
     };
   }
 
